a161cdccb8bd5d199a67a6f7d964a371a93ee389
uttam <uttampun62@gmail.com>
2025-01-03 15:22:29 +0545

    chore: fix getData Error

--- portfolio-frontend/app/hooks/useApi.tsx
+++ portfolio-frontend/app/hooks/useApi.tsx
@@ -1,44 +1,47 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from "react";
 
 interface ApiResponse<T> {
-    data: T
+  data: T;
 }
-export default function useApi<T>(apiUrl:string){
-     const [fetchData, setData] = useState<T|null>(null);
-   
-     const getData = async() =>{
-        try {
-             const response = await axios.get<ApiResponse<T>>(apiUrl);
-             setData(response.data.data);
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                console.error('Error fetching data:', error.message);
-              } else {
-                console.error('Unknown error occurred while fetching data.');
-              }
-        }
-     }
-    
-     const postData = async (payload: T) =>{
-        try {
-            const response = await axios.post<ApiResponse<T>>(apiUrl, payload);
 
-            return response.data
+export default function useApi<T>(apiUrl: string) {
+  
+  const [fetchData, setData] = useState<T | null>(null);
 
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                console.error('Error fetching data:', error.message);
-              } else {
-                console.error('Unknown error occurred while fetching data.');
-              }
-        }
-     }
-    
-     useEffect(() =>{
-         getData()
-    }, [apiUrl])
-    
-    return {fetchData, setData, getData, postData}
+
+  const getData = async () => {
+    try {
+      const response = await axios.get<ApiResponse<T>>(apiUrl);
+      setData(response.data.data); 
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error('Error fetching data:', error.message);
+      } else {
+        console.error('Unknown error occurred while fetching data.');
+      }
+    }
+  };
+
+ 
+  const postData = async (payload: T): Promise<ApiResponse<T> | undefined> => {
+    try {
+      const response = await axios.post<ApiResponse<T>>(apiUrl, payload);
+      return response.data; 
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error('Error posting data:', error.message);
+      } else {
+        console.error('Unknown error occurred while posting data.');
+      }
+      return undefined; 
+    }
+  };
+
+  
+  useEffect(() => {
+    getData();
+  }, [apiUrl]);
+
+  return { fetchData, setData, getData, postData };
 }
