cfa4365edbc136da50cf1777b737ef2750c59029
uttam pun <uttampun50@gmail.com>
2025-01-05 21:20:28 +0545

    fix issue dashboard

--- portfolio-frontend/app/dashboard/page.tsx
+++ portfolio-frontend/app/dashboard/page.tsx
@@ -1,15 +1,10 @@
     "use client"
-   
-import { useSelector } from "react-redux"
 
 export default function Dashboard(){
 
-   const token = useSelector((state:any) => state.auth.token)
-
-   console.log(token);
     return(
         <>
-        dasdsa
+       
         </>
     )
 }
