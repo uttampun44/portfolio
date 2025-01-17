80d548a79c4b25ac60d256aff30e135d8ee88ead
uttam pun <uttampun50@gmail.com>
2025-01-17 21:35:50 +0545

    chore: remove string

--- portfolio-frontend/app/dashboard/blogs/page.tsx
+++ portfolio-frontend/app/dashboard/blogs/page.tsx
@@ -26,7 +26,7 @@ type blogPost = {
 }
 
 
-export default function Blogs({ placeholder }: { placeholder: string }) {
+const  Blogs = ({ placeholder }: { placeholder: string }) => {
 
 
     const token = Cookies.get("token");
@@ -184,3 +184,5 @@ export default function Blogs({ placeholder }: { placeholder: string }) {
         }
     </div>;
 }
+
+export default Blogs;
