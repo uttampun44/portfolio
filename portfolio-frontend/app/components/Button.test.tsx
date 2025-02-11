11009d3b984852dca4227b46faf58f1280a15bc2
uttam pun <uttampun50@gmail.com>
2025-02-11 20:22:25 +0545

    SCRUM-BUTTON-COMPONENT: added comment

--- portfolio-frontend/app/components/Button.test.tsx
+++ portfolio-frontend/app/components/Button.test.tsx
@@ -3,6 +3,7 @@ import Button from './Button';
 import { describe, it, expect } from 'vitest';
 import "@testing-library/jest-dom";
 
+// render the button component and test its functionality using the testing library
 describe('Button', () => {
   it('should render button', () => {
     render(<Button>Button</Button>);
